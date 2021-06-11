52178ac8ecb184857bcc13c172620baf1e3b95b9
Peter Combee <petercombee@gmail.com>
2021-06-11 12:28:49 +0200

    Added ListFeatures to showcase new List features

---
+++ Shared/ListFeatures.swift
@@ -0,0 +1,32 @@
+//
+//  Created by Peter Combee on 11/06/2021.
+//
+
+import SwiftUI
+
+struct ListFeatures: View {
+    var body: some View {
+        List {
+            Section {
+                ForEach(0..<20) { index in
+                    Text("Row \(index)")
+                        .listRowSeparatorTint(.green)
+                }
+                .listSectionSeparatorTint(.red)
+            }
+            Section {
+                ForEach(0..<10) { index in
+                    Text("Row \(index)")
+                        .listRowSeparator(.hidden)
+                }
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
+    }
+}
+
+struct ListFeatures_Previews: PreviewProvider {
+    static var previews: some View {
+        ListExample()
+    }
+}
